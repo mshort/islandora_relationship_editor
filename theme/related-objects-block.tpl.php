f9fe6c6f468b42fca8774b44ca1d8b05f6c9242a
Matthew Short <mshort@niu.edu>
2015-02-16 14:27:04 -0600

    first commit

--- theme/related-objects-block.tpl.php
+++ theme/related-objects-block.tpl.php
@@ -13,7 +13,7 @@
 
     <?php if($related): ?>
       <div>
-<!--        <h2><?php print t('Related Objects'); ?></h2> -->
+        <h2><?php print t('Related Objects'); ?></h2>
         <ul>
           <?php foreach ($related as $obj): ?>
             <li><?php print l($obj->label, "islandora/object/{$obj->id}"); ?></li>
@@ -21,16 +21,6 @@
         </ul>
       </div>
     <?php endif; ?>
-
-<HR>
-<a class="colorbox-inline" href="?height=500&inline=true#id-of-content">Current ontology</a>
-
-<div style="display: none;">
-<div id="id-of-content">
-<?php print $list_ontology; ?>
-</div>
-</div>
-
   </div>
 </div>
 
